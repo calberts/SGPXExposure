c93b3b99068e44632170474ad3505cc231ec6c34
Alberts, C. (Chris) <chris.alberts@ing.com>
2020-04-18 12:48:23 +0200

    Update the script with with some comments

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SGPXExposure
+
+
+Enter your Input  in the following format
+Enter exposure time in sec: 300
+Enter start Time: "18 22:30"       # use " "
+Enter End   Time: "19 01:45"       # use " "
+
+In the script you can change your foucus time
+so the amount of exposure become more precise
+
+FocusTime = 4         # Minuten
+StartedGuiding = 1    # Minuten
+TimeforFlip = 6       # Minuten
--- SGPxExposure.py
+++ SGPxExposure.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+# Update 18 April 2020
 print('-------------------------------------')
-print()
+print
 print("Enter your Input")
-ExposureTimeSec =  input("Enter exposure time in sec: ")# Seconds
+ExposureTimeSec =  input("Enter exposure time in sec: ")     # Seconds
 ExposureTime = int(ExposureTimeSec) / 60
 StartTime = input("Enter start Time: ")
-EndTime =   input("Enter End   Time: ")
+EndTime =  input("Enter End   Time: ")
 
+# Personal Settings
 FocusTime = 4         # Minuten
 StartedGuiding = 1    # Minuten
 TimeforFlip = 6       # Minuten
@@ -26,16 +28,14 @@ else:
 td_mins      = int(round(td.total_seconds() / 60))
 td_exposures = int(round(td_mins / ExposureTime))
 NumberFocus  =  td_exposures / 12
+print
 print("Amount of Focus Actions        = " + str(round(NumberFocus)))
 td_hours     = int((td_mins / 60))
 td_other     = (NumberFocus*FocusTime) + StartedGuiding + TimeforFlip
 td_mins      = td_mins - td_other
 td_exposures = int(round(td_mins / ExposureTime))
-print()
 print('The difference is approx       = %s minutes' % round(td_mins))
-print('The difference is approx       = %s hours' % td_hours)
+#print('The difference is approx       = %s hours' % td_hours)
 print('Expected ' + str(ExposureTimeSec)+' Sec' + ' Exposures     = ' + str(td_exposures))
-print()
-print()
-print()
+print
 print('-------------------------------------')
