6d5bc055671a20ac502ef1a3a656ec101ca0b841
Alberts, C. (Chris) <chris.alberts@ing.com>
2020-04-18 12:13:07 +0200

    Initial Version of SGPxExposures estimation

---
+++ SGPxExposure.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+print('-------------------------------------')
+print()
+print("Enter your Input")
+ExposureTimeSec =  input("Enter exposure time in sec: ")# Seconds
+ExposureTime = int(ExposureTimeSec) / 60
+StartTime = input("Enter start Time: ")
+EndTime =   input("Enter End   Time: ")
+
+FocusTime = 4         # Minuten
+StartedGuiding = 1    # Minuten
+TimeforFlip = 6       # Minuten
+
+ExposuretimeMin = ExposureTime / 60
+
+#fmt = '%d-%m-%Y %H:%M'
+fmt = '%d %H:%M'
+tstamp1 = datetime.strptime(StartTime, fmt)
+tstamp2 = datetime.strptime(EndTime, fmt)
+
+if tstamp1 > tstamp2:
+    td = tstamp1 - tstamp2
+else:
+    td = tstamp2 - tstamp1
+
+td_mins      = int(round(td.total_seconds() / 60))
+td_exposures = int(round(td_mins / ExposureTime))
+NumberFocus  =  td_exposures / 12
+print("Amount of Focus Actions        = " + str(round(NumberFocus)))
+td_hours     = int((td_mins / 60))
+td_other     = (NumberFocus*FocusTime) + StartedGuiding + TimeforFlip
+td_mins      = td_mins - td_other
+td_exposures = int(round(td_mins / ExposureTime))
+print()
+print('The difference is approx       = %s minutes' % round(td_mins))
+print('The difference is approx       = %s hours' % td_hours)
+print('Expected ' + str(ExposureTimeSec)+' Sec' + ' Exposures     = ' + str(td_exposures))
+print()
+print()
+print()
+print('-------------------------------------')
